f2014e62d7269b01322878fbba48f3556945dc2a
arvagas <arvagas@gmail.com>
2021-08-16 11:42:15 -0400

    created setter anf getter methods

--- src/employeeApp/Employee.java
+++ src/employeeApp/Employee.java
@@ -12,7 +12,12 @@ public class Employee {
   private int companyId;
   private int healthPlanId;
 
-  public Employee(String fname, String lname, double salary, boolean has401k, int companyId, int healthPlanId) {
+  public Employee(String fname,
+                  String lname,
+                  double salary,
+                  boolean has401k,
+                  int companyId,
+                  int healthPlanId) {
     id = maxId;
     maxId++;
 
@@ -22,5 +27,58 @@ public class Employee {
     this.has401k = has401k;
     this.companyId = companyId;
     this.healthPlanId = healthPlanId;
+  }
+
+  //Getters and Setters
+  public String getFname() {
+    return fname;
+  }
+
+  public void setFname(String fname) {
+    this.fname = fname;
+  }
+  
+  public String getLname() {
+    return lname;
+  }
+
+  public void setLname(String lname) {
+    this.lname = lname;
+  }
+
+  public double getSalary() {
+    return fname;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+  public boolean getHas401k() {
+    return has401k;
+  }
+
+  public void setHas401k(boolean has401k) {
+    this.has401k = has401k;
+  }
+
+  public int getCompanyId() {
+    return companyId;
+  }
+
+  public void setCompanyId(int companyId) {
+    this.companyId = companyId;
+  }
+  
+  public int getHealthPlanId() {
+    return healthPlanId;
+  }
+
+  public void setHealthPlanId(int healthPlanId) {
+    this.healthPlanId = healthPlanId;
+  }
+
+  public int getId() {
+    return id;
   }
 }
