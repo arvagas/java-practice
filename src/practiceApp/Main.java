d85dbd26f6c8a73a0c5e4852b8478a64ca47b09b
arvagas <arvagas@gmail.com>
2021-08-16 11:00:04 -0400

    Initial commit

---
+++ src/practiceApp/Main.java
@@ -0,0 +1,11 @@
+package practiceApp;
+
+public class Main {
+  public static void main(String[] args) {
+    workWithData();
+  }
+
+  private static void workWithData() {
+    System.out.println("Welcome to Java!");
+  }
+}
